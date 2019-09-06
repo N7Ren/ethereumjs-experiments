a7f6e8c7e795e96fe9ebdf6992fa073c45f665ca
RemoteRen <ren@remoteren.com>
2019-09-06 21:46:24 +0200

    1st experiment with promises instead of callbacks

---
+++ src/read-account-state-and-storage-promisified.js
@@ -0,0 +1,81 @@
+const level = require('level')
+const rlp = require('rlp')
+const utils = require('ethereumjs-util')
+
+const Blockchain = require('ethereumjs-blockchain').default
+const Trie = require('merkle-patricia-tree/secure')
+const Account = require('ethereumjs-account').default
+const BN = utils.BN
+
+const db = level('/Volumes/2nd/geth/ethereum/geth/chaindata') //path to (ropsten) geth db
+const address = '0x7ac337474ca82e0f324fbbe8493f175e0f681188' //random ropsten contract
+const blockNumber = 19693
+
+console.log(`Getting information for Account ${address} at block number: ${blockNumber}`)
+console.log()
+
+const bc = new Blockchain({ db: db })
+
+start()
+
+async function start () {
+  try {
+    const block = await getBlock(blockNumber)
+    const trie = new Trie(db, block.header.stateRoot)
+    const state = await getState(trie, address)
+
+    if (state != null) {
+      const acc = new Account(state)
+      const storageRoot = acc.stateRoot
+
+      console.log('Account fields: ')
+      console.log('nonce: ' + utils.bufferToInt(acc.nonce))
+      console.log('balance: ' + new BN(acc.balance))
+      console.log('storageRoot: ' + utils.bufferToHex(storageRoot))
+      console.log('codeHash: ' + utils.bufferToHex(acc.codeHash))
+      console.log()
+
+      console.log('Storage trie contents for account: ')
+      const storageTrie = trie.copy()
+      storageTrie.root = storageRoot
+
+      storageTrie.createReadStream()
+        .on('data', function (data) {
+          console.log('key: ' + utils.bufferToHex(data.key))
+          console.log('value: ' + utils.bufferToHex(rlp.decode(data.value)))
+        })
+        .on('end', function () {
+          console.log('Done reading storage trie.')
+        })
+    }    
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// getBlock as Promise
+function getBlock (blocknr) {
+  return new Promise((resolve, reject) => {
+    bc.getBlock(
+      blocknr, (err, block) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(block)
+        }
+      })
+  })
+}
+
+// trie.get as Promise
+function getState (trie, address) {
+  return new Promise((resolve, reject) => {
+    trie.get(address, function (err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
